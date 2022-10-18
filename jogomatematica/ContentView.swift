85172dad5bd01c544b923a5b45048bd871db4cdd
Natália Pessoa de Azevedo Albuquerque <npaa@192.168.1.11>
2022-10-18 11:38:33 -0300

    telinha

--- jogomatematica/ContentView.swift
+++ jogomatematica/ContentView.swift
@@ -1,46 +1,67 @@
-//
-//  ContentView.swift
-//  jogomatematica
-//
-//  Created by Natália Pessoa de Azevedo Albuquerque on 18/10/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var offset = CGSize.zero
-//    @State private var dragOffSet = CGSize.zero
-    @State private var rectPosition = CGPoint(x: 50, y: 650)
+    //    @State private var offset = CGSize.zero
+    //    @State private var dragOffSet = CGSize.zero
+    @State private var rectPosition = CGPoint(x: 50, y: 500)
     @State private var isEnded = false
     @State var scale: CGFloat = 1
     
+    func apertouBotao(){
+        print("apertou botao")
+    }
     var body: some View {
-        VStack{
-            Spacer()
-            Image("crab")
+        ZStack {
+            Image("BlueBrackground")
                 .resizable()
-                .padding()
-                .frame(width: 164.71, height: 100.0)
-                .scaledToFit()
-//                .offset(x: offset.width + dragOffSet.width)
-                .position(rectPosition)
-                .gesture(
-                    DragGesture()
-                        .onChanged{ value in
-                            self.rectPosition = CGPoint(x: value.location.x, y: 650)
-                        }
-                        .onEnded{ value in
-                            self.isEnded = value.location.x < 120
-                        }
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                ZStack{
+                    Image("bluerectangle")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: 414, height: 190)
                     
-                )
+                    HStack{
+                        Button(action: {
+                            apertouBotao()
+                        }) {
+                            Image("botaohome")
+                        }
+                        
+                        Image("goal2")
+                        
+                        Button(action: {
+                            apertouBotao()
+                        }) {
+                            Image("botaopausa")
+                        }
+                    }
+                }
+                
+                Image("crab")
+                    .resizable()
+                    .frame(width: 164.71, height: 100.0)
+                    .scaledToFit()
+                //                .offset(x: offset.width + dragOffSet.width)
+                    .position(rectPosition)
+                    .gesture(
+                        DragGesture()
+                            .onChanged{ value in
+                                self.rectPosition = CGPoint(x: value.location.x, y: 500)
+                            }
+                            .onEnded{ value in
+                                self.isEnded = value.location.x < 120
+                            }
+                    )
+            }
         }
-        .padding()
+        
     }
     
     func screenBounds() -> CGFloat {
         let max = UIScreen.main.bounds.width
-//        let currentBound = abs(offset.width)
+        //        let currentBound = abs(offset.width)
         return  max
     }
 }
