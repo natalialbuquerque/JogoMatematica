c1f00a1e51553621fb1417812ef0d427f7aa0886
Natália Pessoa de Azevedo Albuquerque <npaa@192.168.1.6>
2022-10-24 13:00:56 -0300

    bolha caindo

--- jogomatematica/ContentView.swift
+++ jogomatematica/ContentView.swift
@@ -1,47 +1,75 @@
 import SwiftUI
 
+struct playButton: View {
+    @Binding var position: CGSize
+    var body: some View {
+        Button("Playbutton") {
+            
+        }
+    }
+}
 struct ContentView: View {
+        @State private var dropping = -470
+    
         @State private var offset = CGSize.zero
         @State private var dragOffSet = CGSize.zero
-    private var caranguejoSize = CGSize(width: UIScreen.main.bounds.width*0.4, height: UIScreen.main.bounds.height*0.2)
     
+        @State private var offSetInicio = CGSize(width: Int.random(in: -170...170), height: -470)
+        @State private var offSetFim = CGSize(width: Int.random(in: -170...170), height: 440)
+    
+    private var caranguejoSize = CGSize(width: UIScreen.main.bounds.width*0.4, height: UIScreen.main.bounds.height*0.2)
     func apertouBotao(){
-        print("apertou botao")
     }
+    
     var body: some View {
         ZStack {
             Image("BlueBrackground")
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
+            
+            Image("bubble")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50)
+                .offset(x: offSetInicio.width, y: CGFloat(dropping))
+                .animation(.easeIn(duration: 3).repeatForever(autoreverses: false))
+                .onAppear() {
+                    dropping = 440
+                }
+            
+                
+            
             VStack{
                 ZStack{
                     Image("bluerectangle")
                         .resizable()
                         .edgesIgnoringSafeArea(.all)
                         .frame(width: 414, height: 190)
-                    
+
                     HStack{
                         Button(action: {
                             apertouBotao()
                         }) {
                             Image("botaohome")
                         }
-                        
+
                         Image("goal2")
-                        
+
                         Button(action: {
                             apertouBotao()
+                            
                         }) {
                             Image("botaopausa")
                         }
                     }
                 }
+               
                 Spacer()
                 Image("crab")
                     .resizable()
                     .frame(width: 164.71, height: 100.0)
                     .offset(x: offset.width + dragOffSet.width)
-//                    .animation(.easeInOut, value: 100)
+                    .animation(.easeInOut, value: 100)
                     .gesture(
                         DragGesture()
                             .onChanged{ value in
